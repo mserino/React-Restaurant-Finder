8f34c9edf035ceb91d814eb2fac12f32f4923079
Maggie Serino <m.serino@hotmail.it>
2018-07-07 14:57:57 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  renderRestaurants(restaurants) {
+    return(
+      <div className="restaurants">
+      {
+        restaurants.map(restaurant => {
+          return (
+            <div className="restaurant">
+              <h2 className="name">{ restaurant.name }</h2>
+            </div>
+          )
+        })
+      }
+      </div>
+    )
+  }
 
   render() {
+    const restaurants = [{ name: 'Sushi Samba' }, { name: 'The Great Wall' }]
     return (
       <div className="App">
          <h1>Bookish</h1>
-        <div className="restaurants">
-          <div className="restaurant">
-            <h2 className="name">Sushi Samba</h2>
-          </div>
-          <div className="restaurant">
-            <h2 className="name">The Great Wall</h2>
-          </div>
-        </div>
+        { this.renderRestaurants(restaurants) }
        </div>
     );
   }
